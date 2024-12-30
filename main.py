6dfd3e200ef5e6479bf0cec42e8537a025e2b8b2
sanikasanikachaudhari071 <145573448+sanikasanikachaudhari071@users.noreply.github.com>
2024-12-30 21:26:18 +0530

    com

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from backend.conversation.chat import chat_with_travel_assistant    
-
+# from backend.memory.chroma_memory.add_data import add_pdf_to_chroma
 if __name__ == "__main__":
     chat_with_travel_assistant()
+    # add_pdf_to_chroma(pdf_path=r"C:\Users\SANIKA CHAUDHARI\OneDrive\Documents\GitHub\StayAI\pdf\Pune - Wikipedia.pdf")
